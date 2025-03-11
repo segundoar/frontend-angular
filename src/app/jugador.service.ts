e92234126387cdc1e778701592f038d80c676adb
Segundo M Arribas Sánchez <segundoarsan@gmail.com>
2025-03-11 21:36:43 +0100

    placeholder reloj arena

--- src/app/jugador.service.ts
+++ src/app/jugador.service.ts
@@ -10,28 +10,28 @@ import { Pagina } from './pagina';
 export class JugadorService {
 
   private baseURL = "http://localhost:8085/api/v1"
-  private baseURL_pro = "https://backend-ajedrez.onrender.com/api/v1"
+  private envPRO = "https://backend-ajedrez.onrender.com/api/v1"
 
   constructor(private httpClient : HttpClient) { }
 
   obtenerListaJugadores(): Observable<Pagina> {
-    return this.httpClient.get<Pagina>(this.baseURL_pro + '/jugadores');
+    return this.httpClient.get<Pagina>(this.envPRO + '/jugadores');
   }
   
   registrarJugador(jugador: Jugador): Observable<Object> {
-    return this.httpClient.post(`${this.baseURL_pro}`, jugador);
+    return this.httpClient.post(`${this.envPRO}`, jugador);
   }
 
   actualizarJugador(id: number, jugador: Jugador): Observable<Object> {
-    return this.httpClient.patch(`${this.baseURL_pro+ '/jugador'}/${id}`, jugador);
+    return this.httpClient.patch(`${this.envPRO+ '/jugador'}/${id}`, jugador);
   }
 
   obtenerJugadorPorId(id: number): Observable<Jugador> {
-    return this.httpClient.get<Jugador>(`${this.baseURL_pro + '/jugador'}/${id}`);
+    return this.httpClient.get<Jugador>(`${this.envPRO + '/jugador'}/${id}`);
   }
 
   eliminarJugador(id: number): Observable<Object> {
-    return this.httpClient.delete(`${this.baseURL_pro}/${id}`);
+    return this.httpClient.delete(`${this.envPRO}/${id}`);
   }
 
 }
